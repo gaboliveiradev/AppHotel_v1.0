d8cee2e4ed26cc10a96c1ee7d3aba2aa037fb410
gaboliveiradev <g38789138@gmail.com>
2022-09-11 16:43:34 -0300

    criando a classe model de CategoriaQuarto

---
+++ AppHotel/Model/CategoriaQuarto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppHotel.Model
+{
+    internal class CategoriaQuarto
+    {
+        public string descricao { get; set; }
+        public double valor_diaria_adulto { get; set; }
+        public double valor_diaria_crianca { get; set; }
+    }
+}

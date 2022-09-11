d4d1be7895d4b4e73f011ad6d16106b8c86e251b
gaboliveiradev <g38789138@gmail.com>
2022-09-11 17:12:09 -0300

    criando a lista de quartos do hotel e mudando a página inicial do app

--- AppHotel/App.xaml.cs
+++ AppHotel/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,11 +8,39 @@ namespace AppHotel
 {
     public partial class App : Application
     {
+        /*
+         * Instanciando a lista de quartos disponiveis no hotel
+        */
+        public List<Model.CategoriaQuarto> quartos = new List<Model.CategoriaQuarto>()
+        {
+            new Model.CategoriaQuarto()
+            {
+                descricao = "Suíte Premium",
+                valor_diaria_adulto = 200.0,
+                valor_diaria_crianca = 100.0
+            },
+
+            new Model.CategoriaQuarto()
+            {
+                descricao = "Suíte Confort Premium",
+                valor_diaria_adulto = 420.0,
+                valor_diaria_crianca = 210.0
+            },
+
+            new Model.CategoriaQuarto()
+            {
+                descricao = "Suíte Deluxe Imperial",
+                valor_diaria_adulto = 700.0,
+                valor_diaria_crianca = 350.0
+            }
+        };
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("pt-BR");
+
+            MainPage = new NavigationPage(new View.OrcamentoHospedagem() );
         }
 
         protected override void OnStart()
